7e96d559e8378fcd4c8022e6b8b612109c38598f
Uladzislau 2 Rezki <uladzislau2.rezki@sonymobile.com>
2017-02-08 09:43:27 +0100

    sched: set loop_max after rq lock is taken
    While doing a load balance there is a race in setting
    loop_max variable since nr_running can be changed causing
    incorect iteration loops.
    
    As a result we may skip some candidates or check the same
    tasks again.
    
    Signed-off-by: Uladzislau 2 Rezki <uladzislau2.rezki@sonymobile.com>
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9693,11 +9693,16 @@ redo:
 		 * correctly treated as an imbalance.
 		 */
 		env.flags |= LBF_ALL_PINNED;
-		env.loop_max  = min(sysctl_sched_nr_migrate, busiest->nr_running);
 
 more_balance:
 		rq_lock_irqsave(busiest, &rf);
 		update_rq_clock(busiest);
+
+		/*
+		 * Set loop_max when rq's lock is taken to prevent a race.
+		 */
+		env.loop_max = min(sysctl_sched_nr_migrate,
+							busiest->nr_running);
 
 		/*
 		 * cur_ld_moved - load moved in current iteration
