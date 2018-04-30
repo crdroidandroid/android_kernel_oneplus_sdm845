686f33f7683eda82ffc43f9a361fc3df15b17e77
Quentin Perret <quentin.perret@arm.com>
2018-04-30 11:23:39 +0100

    EXPERIMENTAL: FROMLIST: sched/topology: Reference the Energy Model of CPUs when available
    The existing scheduling domain hierarchy is defined to map to the cache
    topology of the system. However, Energy Aware Scheduling (EAS) requires
    more knowledge about the platform, and specifically needs to know about
    the span of Performance Domains (PD), which do not always align with
    caches.
    
    To address this issue, use the Energy Model (EM) of the system to extend
    the scheduler topology code with a representation of the PDs, alongside
    the scheduling domains. More specifically, a linked list of PDs is
    attached to each root domain. When multiple root domains are in use,
    each list contains only the PDs covering the CPUs of its root domain. If
    a PD spans over CPUs of two different root domains, it will be
    duplicated in both lists.
    
    The lists are fully maintained by the scheduler from
    partition_sched_domains() in order to cope with hotplug and cpuset
    changes. As for scheduling domains, the list are protected by RCU to
    ensure safe concurrent updates.
    
    The linked lists should be used only from code paths protected by the
    ENERGY_AWARE sched_feat.
    
    Change-Id: I65ba204388315cd3e7048241a7dd1c119021d63e
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    (Applied from
    https://lore.kernel.org/lkml/20180912091309.7551-7-quentin.perret@arm.com/)
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -8,6 +8,7 @@
 #include <linux/sched/deadline.h>
 #include <linux/kernel_stat.h>
 #include <linux/binfmts.h>
+#include <linux/energy_model.h>
 #include <linux/mutex.h>
 #include <linux/psi.h>
 #include <linux/spinlock.h>
@@ -692,6 +693,12 @@ struct max_cpu_capacity {
 	int cpu;
 };
 
+struct perf_domain {
+    struct em_perf_domain *obj;
+    struct perf_domain *next;
+    struct rcu_head rcu;
+};
+
 /*
  * We add the notion of a root-domain which will be used to define per-domain
  * variables. Each exclusive cpuset essentially defines an island domain by
@@ -747,6 +754,13 @@ struct root_domain {
 
 	/* First cpu with maximum and minimum original capacity */
 	int max_cap_orig_cpu, min_cap_orig_cpu;
+
+	/*
+	 * NULL-terminated list of performance domains intersecting with the
+	 * CPUs of the rd. Protected by RCU.
+	 */
+	struct perf_domain	*pd;
+
 };
 
 extern struct root_domain def_root_domain;
@@ -3147,6 +3161,14 @@ struct sched_avg_stats {
 extern void sched_get_nr_running_avg(struct sched_avg_stats *stats);
 
 #define find_first_cpu_bit(...) -1
+
+#ifdef CONFIG_SMP
+#ifdef CONFIG_ENERGY_MODEL
+#define perf_domain_span(pd) (to_cpumask(((pd)->obj->cpus)))
+#else
+#define perf_domain_span(pd) NULL
+#endif
+#endif
 
 #ifdef CONFIG_SMP
 static inline void sched_irq_work_queue(struct irq_work *work)
