a7b8982df58a92c709171fb1c3942af68b3e11d4
Nathan Chancellor <natechancellor@gmail.com>
2018-05-19 17:06:41 -0700

    input: synaptics_driver_s3320: Fix misleading indentation warning
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -2212,7 +2212,7 @@ static ssize_t synap_write_address(struct file *file, const char __user *buffer,
     }
     else
         block = temp_block;
-	return count;
+    return count;
 }
 
 #ifdef SUPPORT_GLOVES_MODE
