b7cf90b79a1ea4437d62a98267fc9ea22460b3a8
Waqar Ali <waqara@nvidia.com>
2018-08-07 11:15:41 -0700

    sched: cpufreq: Reduce overflow possibility
    During the calculation for next frequency, there is a possiblity that
    the intermediate value obtained after multiplying current freq by
    capacity_margin will overflow UINT_MAX. This change-set eliminates that
    possibility by casting freq to a temporary 64-bit variable and doing the
    calculation in 64-bit. The final value is then cast to 32-bit because it
    is guaranteed to be within UINT_MAX range as long as freq is less than
    UINT_MAX / 2 which it always is.
    
    Bug 2106745
    
    Change-Id: If83d6c7104e813bb70bfdf657886d5ca458c35a1
    Signed-off-by: Waqar Ali <waqara@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1794478
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -228,9 +228,21 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
 
-	freq = (((freq * capacity_margin) >> CAPACITY_MARGIN_FLOOR_FACTOR) +
-		freq) * util / max;
+	/*
+	 * Cast freq to 64-bit so that we do not have to check for overflow in
+	 * the calculations later on
+	 */
+	uint64_t tfreq = (uint64_t) freq;
+
+	tfreq = (((tfreq * capacity_margin) >> CAPACITY_MARGIN_FLOOR_FACTOR) +
+			tfreq) * util / max;
 	trace_sugov_next_freq(policy->cpu, util, max, freq);
+
+	/*
+	 * tfreq will always be less than UINT_MAX as long as freq is less than
+	 * UINT_MAX / 2
+	 */
+	freq = (unsigned int) tfreq;
 
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
 		return sg_policy->next_freq;
