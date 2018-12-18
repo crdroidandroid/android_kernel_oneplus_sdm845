5dff289938765ae6b7b5274c0fddf5b02898322e
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-12-18 12:03:25 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I22994886e3a6128e9235e3cf698b0bb405275194
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -11,6 +11,7 @@
  */
 
 #include "sched.h"
+#include "walt.h"
 #include <linux/of.h>
 #include <linux/sched/core_ctl.h>
 #include <trace/events/sched.h>
@@ -26,7 +27,6 @@ unsigned int sysctl_sched_boost;
 static enum sched_boost_policy boost_policy;
 static enum sched_boost_policy boost_policy_dt = SCHED_BOOST_NONE;
 static DEFINE_MUTEX(boost_mutex);
-static unsigned int freq_aggr_threshold_backup;
 static int boost_refcount[MAX_NUM_BOOST_TYPE];
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
@@ -134,8 +134,7 @@ static void _sched_set_boost(int type)
 			boost_refcount[CONSERVATIVE_BOOST] = 0;
 		}
 		if (boost_refcount[RESTRAINED_BOOST] > 0) {
-			update_freq_aggregate_threshold(
-				freq_aggr_threshold_backup);
+			walt_enable_frequency_aggregation(false);
 			boost_refcount[RESTRAINED_BOOST] = 0;
 		}
 		break;
@@ -160,10 +159,8 @@ static void _sched_set_boost(int type)
 
 	case RESTRAINED_BOOST:
 	    boost_refcount[RESTRAINED_BOOST]++;
-		if (boost_refcount[RESTRAINED_BOOST] == 1) {
-			freq_aggr_threshold_backup =
-			    update_freq_aggregate_threshold(1);
-		}
+		if (boost_refcount[RESTRAINED_BOOST] == 1)
+			walt_enable_frequency_aggregation(true);
 		break;
 
 	case FULL_THROTTLE_BOOST_DISABLE:
@@ -189,8 +186,7 @@ static void _sched_set_boost(int type)
 		if (boost_refcount[RESTRAINED_BOOST] >= 1) {
 			boost_refcount[RESTRAINED_BOOST]--;
 			if (!boost_refcount[RESTRAINED_BOOST])
-				update_freq_aggregate_threshold(
-					freq_aggr_threshold_backup);
+				walt_enable_frequency_aggregation(false);
 		}
 		break;
 
