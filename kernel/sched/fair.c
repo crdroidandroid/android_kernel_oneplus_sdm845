d1fe74e8febb531f7dbbef965b4003c242b09d96
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-03-22 11:40:00 +0530

    sched/fair: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: Ice00d43c70be105b5bc3ad09caa6047aa98d7402
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [clingutla@codeaurora.org: Resolved trivial merge conflicts.]
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10558,6 +10558,14 @@ static int idle_balance(struct rq *this_rq)
 		force_lb = true;
 
 	/*
+	 * Force higher capacity CPUs doing load balance, when the lower
+	 * capacity CPUs has some misfit tasks.
+	 */
+	if (!is_min_capacity_cpu(this_cpu) && min_cap_cluster_has_misfit_task())
+		force_lb = true;
+
+
+	/*
 	 * We must set idle_stamp _before_ calling idle_balance(), such that we
 	 * measure the duration of idle_balance() as idle time.
 	 */
