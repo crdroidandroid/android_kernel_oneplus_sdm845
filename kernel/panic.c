015df2301c4e57c08bc227a5b7cb856435be3c99
UtsavisGreat <utsavbalar1231@gmail.com>
2019-12-02 13:27:44 +0000

    kernel: fs sync work in panic
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: I82f75e707abcc757909f3906eae143424b2c8e1e
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -2,6 +2,7 @@
  *  linux/kernel/panic.c
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
+ *  Copyright (C) 2019 XiaoMi, Inc.
  */
 
 /*
@@ -155,6 +156,12 @@ void panic(const char *fmt, ...)
 	int old_cpu, this_cpu;
 	bool _crash_kexec_post_notifiers = crash_kexec_post_notifiers;
 	char *function_name;
+
+	if (!in_atomic())
+	{
+		pr_emerg("sys_sync:try sys_sync in panic\n");
+		exec_fs_sync_work();
+	}
 
 	trace_kernel_panic(0);
 
