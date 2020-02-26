793298fb9d501b85995d6371e59d96289c587bc7
DennySPB <dennyspb@gmail.com>
2020-02-26 09:17:41 +0300

    Revert "ANDROID: sched: Disable find_best_target() by default"
    random hungs and after non-boot issue
    
    This reverts commit f9dfcdcbd2d438bc880c683445b20a1b71419f52.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -101,7 +101,7 @@ SCHED_FEAT(ENERGY_AWARE, false)
 /*
  * Fast pre-selection of CPU candidates for EAS.
  */
-SCHED_FEAT(FIND_BEST_TARGET, false)
+SCHED_FEAT(FIND_BEST_TARGET, true)
 
 /*
  * Energy aware scheduling algorithm choices:
