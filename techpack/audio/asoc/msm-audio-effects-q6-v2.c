85a743e84b66cfbb41792e19600678a17b5c2e34
Sandhya Mutha Naga Venkata <quic_smuthana@quicinc.com>
2023-06-13 16:08:57 +0530

    ASoC: msm-audio-effects-q6-v2: Add BPF order check
    Added check for bandpassfilter order in order to avoid
    coeff len going out of bounds thereby leading to
    memory overflow issues.
    
    Change-Id: I401f5f38a3d54d9d6af66e770d03629ed5e1a2bd
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>
    (cherry picked from commit a225b1346d434fb0ceec5c81718879f402d31373)
    (cherry picked from commit 83eea8aa0fd343f103d19168f31314717fa5f3aa)

--- techpack/audio/asoc/msm-audio-effects-q6-v2.c
+++ techpack/audio/asoc/msm-audio-effects-q6-v2.c
@@ -1,13 +1,6 @@
-/* Copyright (c) 2013-2017, 2019 The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/slab.h>
@@ -943,6 +936,13 @@ int msm_audio_effects_pbe_handler(struct audio_client *ac,
 				length < PBE_CONFIG_PARAM_LEN ||
 				index_offset != 0) {
 				pr_err("no valid params, len %d\n", length);
+				rc = -EINVAL;
+				goto invalid_config;
+			}
+			if ((pbe->config.bandpass_filter_order > 3) ||
+				(pbe->config.bandpass_filter_order < 1)) {
+				pr_err("%s: Invalid BPF order\n",
+					__func__);
 				rc = -EINVAL;
 				goto invalid_config;
 			}
