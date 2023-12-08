90d05ea3576f2abe768de4bc329f80f41b3b3299
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-12-08 11:19:00 -0800

    treewide: Clean up misleading indentation warnings for clang 17.0.4

--- drivers/media/platform/msm/camera_oneplus/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
+++ drivers/media/platform/msm/camera_oneplus/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -1108,18 +1108,16 @@ static int cam_ife_hw_mgr_acquire_res_ife_csid_rdi(
 		out_port = &in_port->data[i];
 		if (!cam_ife_hw_mgr_is_rdi_res(out_port->res_type))
 			continue;
-
-			/* get cid resource */
-			rc = cam_ife_mgr_acquire_cid_res(ife_ctx,
-				in_port, &cid_res_id,
-				cam_ife_hw_mgr_get_ife_csid_rdi_res_type(
-				out_port->res_type));
-			if (rc) {
-				CAM_ERR(CAM_ISP,
-					"Acquire IFE CID resource Failed");
-				goto err;
+		/* get cid resource */
+		rc = cam_ife_mgr_acquire_cid_res(ife_ctx,
+			in_port, &cid_res_id,
+			cam_ife_hw_mgr_get_ife_csid_rdi_res_type(
+			out_port->res_type));
+		if (rc) {
+			CAM_ERR(CAM_ISP,
+				"Acquire IFE CID resource Failed");
+			goto err;
 		}
-
 		rc = cam_ife_hw_mgr_get_res(&ife_ctx->free_res_list,
 			&csid_res);
 		if (rc) {
