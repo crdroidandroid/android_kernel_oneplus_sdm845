0f221fc75afeb9babd2e2017ff9d8b9fc92089da
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-02-09 23:01:52 -0800

    [SQUASH] Revert PEWQs
    Does that actually help anything on a CAF kernel?

--- drivers/gpu/drm/drm_probe_helper.c
+++ drivers/gpu/drm/drm_probe_helper.c
@@ -159,8 +159,7 @@ void drm_kms_helper_poll_enable_locked(struct drm_device *dev)
 	}
 
 	if (poll)
-		queue_delayed_work(system_power_efficient_wq,
-				   &dev->mode_config.output_poll_work, delay);
+		schedule_delayed_work(&dev->mode_config.output_poll_work, delay);
 }
 EXPORT_SYMBOL(drm_kms_helper_poll_enable_locked);
 
@@ -275,7 +274,7 @@ int drm_helper_probe_single_connector_modes(struct drm_connector *connector,
 		 */
 		dev->mode_config.delayed_event = true;
 		if (dev->mode_config.poll_enabled)
-			mod_delayed_work(system_power_efficient_wq,
+			mod_delayed_work(system_wq,
 					 &dev->mode_config.output_poll_work,
 					 0);
 	}
@@ -465,8 +464,7 @@ out:
 		drm_kms_helper_hotplug_event(dev);
 
 	if (repoll)
-		queue_delayed_work(system_power_efficient_wq,
-				   delayed_work, DRM_OUTPUT_POLL_PERIOD);
+		schedule_delayed_work(delayed_work, DRM_OUTPUT_POLL_PERIOD);
 }
 
 /**
